e0c78c37cbd390110c33a9c5c035fee6e73d07de
Matheus H. <santosmatheussilva375@gmail.com>
2026-09-02 08:53:55 -0300

    Fix formatting issues in Mago.java

--- aula4/Mago.java
+++ aula4/Mago.java
@@ -3,18 +3,15 @@ package aula4;
 public class Mago extends Personagem {
     private int mana;
 
-    // Construtor
     public Mago(String nome, int vida, int nivel) {
         super(nome, vida, nivel);
         setMana(50);
     }
 
-    // Getter da mana
     public int getMana() {
         return mana;
     }
 
-    // Setter da mana com validação
     public void setMana(int mana) {
         if (mana < 0) {
             System.out.println("Erro: a mana não pode ser negativa.");
@@ -24,7 +21,6 @@ public class Mago extends Personagem {
         this.mana = mana;
     }
 
-    // Sobrescrevendo ficha()
     @Override
     public void ficha() {
         super.ficha();
